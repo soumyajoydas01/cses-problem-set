e022e757e4bc10a5bc82cb4dc09dd56878bf91f9
Soumyajoy Das <70829126+soumyajoydas01@users.noreply.github.com>
2021-09-22 23:23:40 +0530

    Add files via upload

---
+++ CSES Problem Set/Introductory Problems/Weird Algorithm/src/code/java/weirdAlgo.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class weirdAlgo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.print(n+" ");
+
+        while (n>1)
+    {
+        if(n%2==0){
+            n/=2;
+            System.out.print(n+" ");
+        }else{
+            n=(n*3)+1;
+            System.out.print(n+" ");
+        }
+    }
+
+    sc.close();
+
+    }
+    
+}
